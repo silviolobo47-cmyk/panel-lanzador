2ce8811ccc6976d3cca5d1a73f58a32bf3e69357
silviolobo47-cmyk <silviolobo47@gmail.com>
2026-09-13 11:57:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "panel-lanzador-v1";
+const CACHE = "panel-lanzador-v2";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
